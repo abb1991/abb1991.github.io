0d7e09447c3e2cdc6fd611119a8505388a62b257
abb1991 <alexbills1991@gmail.com>
2017-04-06 20:29:06 -0700

    added contact div with toggle hide/show, gradient background to image gallery, media query for cotact div.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,18 @@
-let main = function() {
 
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-      acc[i].onclick = function(){
-          /* Toggle between adding and removing the "active" class,
-          to highlight the button that controls the panel */
-          this.classList.toggle("active");
-
-          /* Toggle between hiding and showing the active panel */
-          var panel = this.nextElementSibling;
-          if (panel.style.display === "block") {
-              panel.style.display = "none";
-          } else {
-              panel.style.display = "block";
-          }
-      }
-  }
-
+function showContact() {
+  event.preventDefault();
+  $('#contact').slideToggle('fast');
 }
 
-$('document').ready(main);
+
+$("*").each( function () {
+    var $this = $(this);
+    if (parseInt($this.css("fontSize")) < 12) {
+        $this.css({ "font-size": "12px" });
+    }
+});
+
+console.log('me')
+
+
+
